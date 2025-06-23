21621ee85e899fcf7ec468de3617e3bd21876809
Stoyan Peev <114337044+sspeev@users.noreply.github.com>
2025-06-23 20:32:58 +0300

    Add Cocktails component and integrate it into App; implement GSAP animations for cocktail display

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from '../src/components/Navbar.jsx';
 import Hero from '../src/components/Hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,7 +11,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className='h-dvh bg-black'></div>
+      <Cocktails />
     </main>
   )
 }
